70ca762de13fac6cc202f066b836aaad502f6e1d
Olagundoye Emmanuel <eoluwakayode34@gmail.com>
2020-07-13 01:12:34 +0100

    skeleton projext

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+
+      $('.slider').slider({
+        fullWidth: true,
+        indicators: false,
+        height: 500
+
+      });
+  
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
